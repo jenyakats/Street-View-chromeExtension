2ba735ea4bce213926a8b7f4a309f4ac22f7d53e
Julien Benatar <contact@julienbenatar.com>
2017-05-15 21:45:33 -0400

    Find the closest panorama in a 2km radius up to three times, or get tacos

--- load.js
+++ load.js
@@ -1,16 +1,43 @@
+var maxTries = 3;
 
-  var panorama;
-  function initialize() {
-    picked = all[Math.floor(Math.random()*all.length)]
-    // debugger
-    panorama = new google.maps.StreetViewPanorama(
+function initialize() {
+  var picked = all[Math.floor(Math.random()*all.length)];
+  var streetViewService = new google.maps.StreetViewService();
+
+  streetViewService.getPanorama(
+    {
+      location: new google.maps.LatLng(picked[0], picked[1]),
+    // It will return the nearest panorama when the
+    // given radius is 2000 meters or less.
+    // This way, it should always return something 🏄
+      radius: 2000
+    }, function(data, status) {
+      if (status === 'OK') {
+        var panorama = new google.maps.StreetViewPanorama(
         document.getElementById('street-view'),
         {
-          // position: {lat: one.lat, lng: one.lng},
-          position: {lat: picked[0], lng: picked[1]},
-          // position: {lat: 21.46225, lng: -158.00366},
+          position: data.location.latLng,
           pov: {heading: 165, pitch: 0},
-          // status: "OK"
           zoom: 1
         });
-  }
+      } else {
+          // Try up to three times with random coordinates
+          // If not, redirecting the browser to what it needs: tacos
+          if (maxTries-- > 0) {
+            initialize();
+          } else {
+            getTacos();
+          }
+      }
+  });
+}
+
+function getTacos() {
+  var losHermanosId = 'B7aVSGDC5YzTrbhy9MzXyA';
+  var panorama = new google.maps.StreetViewPanorama(
+    document.getElementById('street-view'),
+    {
+      pano: losHermanosId,
+      zoom: 1
+    });
+}
