1cdbdfe1a55f51ef0122ae03c38437d9c9624300
Jenny Kats <jenny.kats@aol.com>
2020-07-06 11:20:00 -0400

    update load

--- load.js
+++ load.js
@@ -1,5 +1,3 @@
-var maxTries = 3;
-
 function initialize() {
   var picked = all[Math.floor(Math.random()*all.length)];
   var streetViewService = new google.maps.StreetViewService();
@@ -9,7 +7,7 @@ function initialize() {
       location: new google.maps.LatLng(picked[0], picked[1]),
     // It will return the nearest panorama when the
     // given radius is 2000 meters or less.
-    // This way, it should always return something 🏄
+    // This way, it should always return something
       radius: 2000
     }, function(data, status) {
       if (status === 'OK') {
@@ -21,23 +19,7 @@ function initialize() {
           zoom: 1
         });
       } else {
-          // Try up to three times with random coordinates
-          // If not, redirecting the browser to what it needs: tacos
-          if (maxTries-- > 0) {
             initialize();
-          } else {
-            getTacos();
-          }
       }
   });
-}
-
-function getTacos() {
-  var losHermanosId = 'B7aVSGDC5YzTrbhy9MzXyA';
-  var panorama = new google.maps.StreetViewPanorama(
-    document.getElementById('street-view'),
-    {
-      pano: losHermanosId,
-      zoom: 1
-    });
 }
